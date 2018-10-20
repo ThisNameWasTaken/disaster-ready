4383fc486364c4fa4319ccda4d93d4c4a8ad775d
Adrian <adriandinca2008@yahoo.com>
2018-10-20 19:37:56 +0300

    feat: Add material components

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => navigator.serviceWorker.register('/serviceWorker.js'));
-}
+import { MDCTopAppBar } from '@material/top-app-bar';
+import { MDCDrawer } from '@material/drawer';
+import { MDCTextField } from '@material/textfield';
+
+new MDCTextField(document.querySelector('.mdc-text-field'));
+
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => navigator.serviceWorker.register('/serviceWorker.js'));
+// }
+
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+const drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
+
+topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+});
