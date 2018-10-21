124a3e3c3c751ca8beb569c31954cc63c7da71bb
Adrian <adriandinca2008@yahoo.com>
2018-10-21 04:53:05 +0300

    feat: Add backpack section

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
+import './maps';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCDrawer } from '@material/drawer';
 import { MDCTextField } from '@material/textfield';
 import { MDCRipple } from '@material/ripple';
-
-import './maps';
+import { MDCList } from '@material/list';
+import SmoothScroll from 'smooth-scroll';
+import sal from 'sal.js/dist/sal';
 
 document.querySelectorAll('.mdc-button').forEach(button => new MDCRipple(button));
 
 new MDCTextField(document.querySelector('.mdc-text-field'));
+//new MDCTabIndicator(document.querySelector('.mdc-tab-indicator'));
+new MDCList(document.querySelector('.mdc-list'));
 
 // if ('serviceWorker' in navigator) {
 //     window.addEventListener('load', () => navigator.serviceWorker.register('/serviceWorker.js'));
@@ -15,6 +19,10 @@ new MDCTextField(document.querySelector('.mdc-text-field'));
 
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
+
+new SmoothScroll('a[href*="#"]');
+
+sal();
 
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
